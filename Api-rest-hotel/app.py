9c81c869544c9c9c6a7fdace40affafe29ff12cd
Djeferson Almeida <djefersonalmeida20@gmail.com>
2025-06-01 19:37:19 -0300

    feat: JWTMANAGER, /login

--- Api-rest-hotel/app.py
+++ Api-rest-hotel/app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
-from resources.user import User, UserRegister
+from resources.user import User, UserRegister, UserLogin
+from flask_jwt_extended import JWTManager
 from sql_alchemy import banco
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'  #
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'ThisIsAKey'
 api = Api(app)
+jwt = JWTManager(app)
 
 banco.init_app(app)
 
@@ -15,6 +18,7 @@ api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 api.add_resource(User,'/users/<int:user_id>')
 api.add_resource(UserRegister,'/register')
+api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
     with app.app_context():
