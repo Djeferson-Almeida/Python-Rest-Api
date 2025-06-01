3c94df1d41fb0aa97debb1258edd5f371bdedc16
Djeferson Almeida <djefersonalmeida20@gmail.com>
2025-05-31 21:14:40 -0300

    feat: Methods: find_user, find_by_login, save_user, delete_user

--- Api-rest-hotel/models/user.py
+++ Api-rest-hotel/models/user.py
@@ -1,15 +1,15 @@
 from sql_alchemy import banco
 
 class UserModel(banco.Model):
-    __tablename__ =  'users'
+    __tablename__ = 'users'
 
     user_id = banco.Column(banco.Integer, primary_key=True)
-    login = banco.Column(banco.string(40))
-    senha = banco.Column(banco.string(40))
+    login = banco.Column(banco.String(40))
+    password = banco.Column(banco.String(40))
 
-    def __init__(self,login,senha):
+    def __init__(self, login, password):
         self.login = login
-        self.senha = senha
+        self.password = password
 
     def json(self):
         return{
@@ -21,6 +21,11 @@ class UserModel(banco.Model):
     def find_user(cls, user_id):
         return cls.query.filter_by(user_id=user_id).first()
 
+    @classmethod
+    def find_by_login(cls, login):
+        return cls.query.filter_by(login=login).first()
+
+
     def save_user(self):
         banco.session.merge(self)
         banco.session.commit()
@@ -28,6 +33,7 @@ class UserModel(banco.Model):
     def delete_user(self):
         banco.session.delete(self)      
         banco.session.commit() 
+
 
 
 
