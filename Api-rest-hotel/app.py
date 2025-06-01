caaafb44dd579bec5ebd9c0a6d99b77d789b3ade
Djeferson Almeida <djefersonalmeida20@gmail.com>
2025-05-31 21:12:33 -0300

    feat: endpoints: /users, /register

--- Api-rest-hotel/app.py
+++ Api-rest-hotel/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
+from resources.user import User, UserRegister
 from sql_alchemy import banco
 
 app = Flask(__name__)
@@ -12,6 +13,8 @@ banco.init_app(app)
 
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
+api.add_resource(User,'/users/<int:user_id>')
+api.add_resource(UserRegister,'/register')
 
 if __name__ == '__main__':
     with app.app_context():
