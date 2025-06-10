815cfbaa607c6a57ea20fa0eff01fb3eb3522480
Djeferson Almeida <djefersonalmeida20@gmail.com>
2025-06-09 21:24:42 -0300

    feat: Filtros.py added

---
+++ Api-rest-hotel/resources/filtros.py
@@ -0,0 +1,21 @@
+def normalize_path_params(cidade=None, estado=None, estrelas_min=0, estrelas_max=5,
+                          diaria_min=0, diaria_max=1000, limit=50, offset=0, **dados):
+    if cidade:
+        return {
+            'estrelas_min': estrelas_min,
+            'estrelas_max': estrelas_max,
+            'diaria_min': diaria_min,
+            'diaria_max': diaria_max,
+            'cidade': cidade,
+            'estado': estado,
+            'limit': limit,
+            'offset': offset
+        }
+    return {
+        'estrelas_min': estrelas_min,
+        'estrelas_max': estrelas_max,
+        'diaria_min': diaria_min,
+        'diaria_max': diaria_max,
+        'limit': limit,
+        'offset': offset
+    }
